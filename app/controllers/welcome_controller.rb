b2d3effcc2aa609793e35e9403b463823c2f55cf
Harry Boyes <harry.boyes@gmail.com>
2021-09-25 00:12:21 +0100

    Redirect to menu if logged in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,11 @@
 class WelcomeController < ApplicationController
+  before_action :redirect_if_logged_in
+
   def index
     @title = "Let's Climb Some Shit"
+  end
+
+  def redirect_if_logged_in
+    redirect_to menu_path if session[:userinfo].present?
   end
 end
