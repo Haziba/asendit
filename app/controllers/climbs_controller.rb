70a373594ae7dd7ea866544a7189ada979b6230c
Harry Boyes <harryboyes@Harrys-MacBook-Pro.local>
2021-10-23 00:07:28 +0100

    Exclude nonattempted route sets from climb show

--- app/controllers/climbs_controller.rb
+++ app/controllers/climbs_controller.rb
@@ -31,7 +31,7 @@ class ClimbsController < ApplicationController
 
   def show
     @climb = Climb.find(params[:id])
-    climbed_route_sets = Route.where(id: @climb.route_states.map(&:route_id)).map(&:route_set_id).uniq
+    climbed_route_sets = Route.where(id: @climb.route_states.select(&:tried?).map(&:route_id)).map(&:route_set_id).uniq
     @routes = RouteSet.find(climbed_route_sets).map { |route_set| [route_set.id, route_set.routes] }.to_h
     @route_sets = RouteSet.find(@routes.keys)
   end
