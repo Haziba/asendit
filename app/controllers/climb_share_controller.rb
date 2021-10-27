8049c55d54b011a18f8b1c563e9a618de4aec9bf
Harry Boyes <harry.boyes@gmail.com>
2021-10-27 10:51:04 +0100

    Filter out untried route sets from climb_share#show

--- app/controllers/climb_share_controller.rb
+++ app/controllers/climb_share_controller.rb
@@ -1,7 +1,7 @@
 class ClimbShareController < ApplicationController
   def show
     @climb = Climb.find(params[:climb_id])
-    climbed_route_sets = Route.where(id: @climb.route_states.map(&:route_id)).map(&:route_set_id).uniq
+    climbed_route_sets = Route.where(id: @climb.route_states.select(&:tried?).map(&:route_id)).map(&:route_set_id).uniq
     @routes = RouteSet.find(climbed_route_sets).map { |route_set| [route_set.id, route_set.routes] }.to_h
     @route_sets = RouteSet.find(@routes.keys)
   end
