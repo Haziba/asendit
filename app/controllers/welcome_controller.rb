9901f4251ece05bb5380a7520dc4d64145948bcc
Harry Boyes <harry.boyes@gmail.com>
2022-03-03 20:20:34 +0000

    Enable DEV_USER env variable to log in user automatically

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,16 @@
 class WelcomeController < ApplicationController
+  before_action :login_if_in_dev
   before_action :redirect_if_logged_in
 
   def index
     @title = "Let's Climb Some Shit"
+  end
+
+  def login_if_in_dev
+    session[:userinfo] = {
+      "id": ENV['DEV_USER'],
+      "admin": true
+    } unless ENV['DEV_USER'].nil?
   end
 
   def redirect_if_logged_in
