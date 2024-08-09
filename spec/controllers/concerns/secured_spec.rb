f2280dddb5fa44f8ff421673e8566ac9baa175d8
Harry Boyes <harry.boyes@gmail.com>
2024-08-09 16:06:31 +0100

    Add tests for secured concern

---
+++ spec/controllers/concerns/secured_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+# A dummy controller to test the Secured concern
+class SecuredDummyController < ApplicationController
+  include Secured
+
+  def index
+    render plain: 'Success'
+  end
+end
+
+RSpec.describe SecuredDummyController, type: :controller do
+  controller(SecuredDummyController) do
+    # The dummy action to test the concern
+    def index
+      render plain: 'Success'
+    end
+  end
+
+  describe 'before_action :logged_in_using_omniauth?' do
+    context 'when user is logged in' do
+      before do
+        allow(controller).to receive(:session).and_return(userinfo: { "id" => "1234" })
+        get :index
+      end
+
+      it 'allows the request to proceed' do
+        expect(response).to have_http_status(:ok)
+        expect(response.body).to eq('Success')
+      end
+    end
+
+    context 'when user is not logged in' do
+      before do
+        allow(controller).to receive(:session).and_return({})
+        get :index
+      end
+
+      it 'redirects to the root path' do
+        expect(response).to redirect_to('/')
+      end
+    end
+  end
+end
