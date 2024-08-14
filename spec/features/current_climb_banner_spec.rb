360013323ce0e286c749f361e0e673a1d6853926
Harry Boyes <harry.boyes@gmail.com>
2024-08-14 10:02:12 +0100

    Add feature specs for current climb banner

---
+++ spec/features/current_climb_banner_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.feature "Current climb banner", type: :feature do
+  include_context('logged_in')
+
+  let!(:climb) { create(:climb, current: current, climber: 'harry.boyes@gmail.com') }
+
+  before do
+    visit '/menu'
+  end
+
+  context 'when current climb in progress' do
+    let(:current) { true }
+
+    scenario 'current climb link is shown' do
+      expect(page).to have_selector('#goToCurrentClimb')
+      find('#goToCurrentClimb a').click
+      expect(page).to have_current_path("/climbs/#{climb.id}/edit")
+    end
+  end
+
+  context 'when no current climb in progress' do
+    let(:current) { false }
+
+    scenario 'current climb link is shown' do
+      expect(page).to_not have_selector('#goToCurrentClimb')
+    end
+  end
+end
