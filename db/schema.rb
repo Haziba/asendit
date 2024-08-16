5cac05aa44f97c56ae046c243cbf6ad53165fdf4
Harry Boyes <harry.boyes@gmail.com>
2024-08-16 22:19:50 +0100

    Neaten up after rebase, mostly DB related

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_07_150812) do
-
+ActiveRecord::Schema[7.1].define(version: 2023_02_07_150812) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "climbs", force: :cascade do |t|
     t.text "climber"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.boolean "current"
     t.json "route_state_json", default: []
     t.date "climbed_at"
@@ -26,15 +25,15 @@ ActiveRecord::Schema.define(version: 2023_02_07_150812) do
 
   create_table "places", force: :cascade do |t|
     t.text "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "route_sets", force: :cascade do |t|
     t.text "color"
-    t.datetime "added"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "added", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.bigint "place_id"
     t.date "expires_at"
     t.index ["place_id"], name: "index_route_sets_on_place_id"
@@ -44,8 +43,8 @@ ActiveRecord::Schema.define(version: 2023_02_07_150812) do
     t.bigint "climb_id"
     t.bigint "route_id"
     t.integer "status"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["climb_id"], name: "index_route_states_on_climb_id"
     t.index ["route_id"], name: "index_route_states_on_route_id"
   end
@@ -54,18 +53,18 @@ ActiveRecord::Schema.define(version: 2023_02_07_150812) do
     t.bigint "route_set_id"
     t.integer "pos_x"
     t.integer "pos_y"
-    t.datetime "added"
+    t.datetime "added", precision: nil
     t.integer "floor"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["route_set_id"], name: "index_routes_on_route_set_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.text "reference"
     t.bigint "place_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["place_id"], name: "index_users_on_place_id"
   end
 
