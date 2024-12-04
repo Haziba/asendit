1524e7b5698f41e4b19ed64680d7075010cb881a
Harry Boyes <harry.boyes@gmail.com>
2024-12-04 11:49:37 +0000

    Add action cable updates to place grades

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -5,6 +5,10 @@ class Grade < ApplicationRecord
   validates :name, presence: true
   validates :map_tint_colour, presence: true
 
+  after_create_commit -> { broadcast_append_to place }
+  after_update_commit -> { broadcast_replace_to place }
+  after_destroy_commit -> { broadcast_remove_to place }
+
   def active_route_set
     route_sets.sort_by(&:added).last
   end
